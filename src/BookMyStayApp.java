e285d7656fdd5957ddf74be9f98e80dd43cb38e8
nehalganesh75-sys <nehalganesh75>
2026-03-23 10:42:11 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,13 +1,72 @@
+// Abstract class
+abstract class Room {
+    protected int beds;
+    protected double price;
 
+    public Room(int beds, double price) {
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Abstract method
+    public abstract String getType();
+
+    public void displayDetails(int availability) {
+        System.out.println("Room Type: " + getType());
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: $" + price);
+        System.out.println("Available: " + availability);
+        System.out.println("----------------------");
+    }
+}
+
+// Concrete classes
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 50);
+    }
+
+    public String getType() {
+        return "Single Room";
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 90);
+    }
+
+    public String getType() {
+        return "Double Room";
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super(3, 150);
+    }
+
+    public String getType() {
+        return "Suite Room";
+    }
+}
+
+// Main application
 public class BookMyStayApp {
-
     public static void main(String[] args) {
+        // Create room objects (polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        System.out.println("Welcome to Hotel Booking System");
+        // Static availability variables
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-        System.out.println("Application: Hotel Booking System");
-        System.out.println("Version: 1.0");
-
-        System.out.println("Application started successfully.");
+        // Display details
+        single.displayDetails(singleAvailable);
+        doubleRoom.displayDetails(doubleAvailable);
+        suite.displayDetails(suiteAvailable);
     }
 }
