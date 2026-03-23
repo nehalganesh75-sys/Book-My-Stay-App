7e8e486be827b85d21a82bfe287d077793a45207
nehalganesh75-sys <nehalganesh75>
2026-03-23 10:33:16 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Hotel Booking System");
+
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        System.out.println("Application started successfully.");
+    }
+}
